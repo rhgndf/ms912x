9cd83e32a64cfe1395eae1fb418cd7cbf308fc42
rhgndf <16336768+rhgndf@users.noreply.github.com>
2023-11-02 19:52:08 +0800

    Kernel 6.1 support

--- ms912x_drv.c
+++ ms912x_drv.c
@@ -7,7 +7,6 @@
 #include <drm/drm_damage_helper.h>
 #include <drm/drm_drv.h>
 #include <drm/drm_fb_helper.h>
-#include <drm/drm_fbdev_generic.h>
 #include <drm/drm_file.h>
 #include <drm/drm_gem_atomic_helper.h>
 #include <drm/drm_gem_framebuffer_helper.h>
